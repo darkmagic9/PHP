392467f3032b8e1d55563ee43c0e53e8873b10b4
darkmagic9 <darkmagic9@gmail.com>
2022-06-08 22:05:00 +0700

    initial commit

--- Dynamic Menu - User Permission/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con=mysqli_connect('localhost','root','','dynamic_menu');
-
-if(mysqli_connect_errno())
-{
-	echo 'Failed to connect '.mysqli_connect_error();
-}
-?>
--- Dynamic Menu - User Permission/permissiondenied.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<p>You dont have permission to access this page. Please contact your administrative department.</p>
-</body>
-</html>
